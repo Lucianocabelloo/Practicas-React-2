b5f03ae4d8ffcc63cec925e34e9f339f0e0a224a
lucianocabelloo <you@example.com>
2023-12-20 11:20:26 -0300

    Add complete state

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -1,18 +1,46 @@
 /* eslint-disable react/prop-types */
-import {Button, ButtonGroup} from "@nextui-org/react";
+import { Button, ButtonGroup } from "@nextui-org/react";
+import { useState, useEffect } from "react";
 
+const Task = ({ key, taskName, onDelete, onEdit }) => {
+  const [click, setclick] = useState(true);
 
-const Task = ({key, taskName, onDelete, onEdit}) => {
+  const handleChange = () => {
+    setclick(!click);
+    saveLocalStorage(!click);
+
+  };
+
+  const saveLocalStorage = (state) => {
+    localStorage.setItem("StateTask", JSON.stringify(state));
+  };
+
+  const getLocalStorage = () => {
+    const storedTasks = localStorage.getItem("StateTask");
+    return storedTasks ? JSON.parse(storedTasks) : true;
+  };
+
+  useEffect(() => {
+    setclick(getLocalStorage());
+  }, []);
+
+  const containerClassName = click
+    ? "bg-zinc-800 p-3 rounded-md flex items-center justify-between"
+    : "bg-red-900 p-3 rounded-md flex items-center justify-between line-through text-lg";
+
   return (
-    <div className="bg-zinc-800 p-3 rounded-md flex items-center justify-between" key={key}>
-        <h2>{taskName}</h2>
-        <ButtonGroup className="gap-0.3">
-      <Button onClick={onEdit} variant="ghost" color="secondary">Editar</Button>
-      <Button onClick={onDelete} variant="ghost" color="danger">Borrar</Button>
-    </ButtonGroup>
+    <div className={containerClassName} onClick={handleChange} key={key}>
+      <h2>{taskName}</h2>
+      <ButtonGroup className="gap-0.3 ">
+        <Button onClick={onEdit} variant="ghost" color="secondary">
+          Editar
+        </Button>
+        <Button onClick={onDelete} variant="ghost" color="danger">
+          Borrar
+        </Button>
+      </ButtonGroup>
     </div>
-   
-  )
-}
+  );
+};
 
-export default Task
+export default Task;
