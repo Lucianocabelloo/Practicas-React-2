b309777639a3f56b9fb68bb219534f2f516fda96
lucianocabelloo <you@example.com>
2023-12-20 10:48:56 -0300

    Add Msg in TaskList About Tasks

--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -7,10 +7,15 @@ const TaskList = ({ tareas, onEdit, onDelete }) => {
     <>
     <div className='flex flex-col gap-5'>
 
+
     {
+ tareas.length ?
   tareas.map((tarea, index) => (
     <Task key={index} taskName={tarea} onEdit={() => onEdit(index)} onDelete={onDelete} />
   ))
+  :
+  <p>No hay tareas disponibles</p>
+
 }
 
 
